bbea04a172059c209a08a0b0890b05549278c036
MajaTodorovska <maja.todorovska@>
2016-12-16 16:58:52 +0100

    'dshkshg'

--- app.js
+++ app.js
@@ -3,7 +3,45 @@ var workshopApp = angular.module('workshopApp', []);
 workshopApp.controller('workshopController', function workshopController($scope) {
     $scope.step = 1;
     $scope.progress = 6.67;
-
+    $scope.techniques = [
+    	{
+    		index: 1,
+    		name: 'Specialized controls'
+    	},
+    	{
+    		index: 2,
+    		name: 'Disable pressed buttons'
+    	},
+    	{
+    		index: 3,
+    		name: 'Shadows around modal windows'
+    	},
+    	{
+    		index: 4,
+    		name: 'Instructions on empty states'
+    	},
+    	{
+    		index: 5,
+    		name: 'Pressed button states'
+    	},
+    	{
+    		index: 6,
+    		name: 'Sign up link in the log in page'
+    	},
+    	{
+    		index: 7,
+    		name: 'Context sensitive navigation'
+    	},
+    	{
+    		index: 8,
+    		name: 'More emphasis on key functions'
+    	},
+    	{
+    		index: 9,
+    		name: 'Embed video'
+    	}
+    	
+    ]
 	$scope.incrementStep = function () {
 		if ($scope.step < 15) {
 			$scope.step++;
